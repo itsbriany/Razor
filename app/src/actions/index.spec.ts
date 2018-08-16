c07ffa78a8c76bdd9cf8ba665846b25964c309dd
Brian Yip <itsbriany@gmail.com>
2018-08-15 22:42:48 -0300

    Tests are now monitored and added tests for redux actions.

---
+++ app/src/actions/index.spec.ts
@@ -0,0 +1,18 @@
+import * as actions from '.';
+import * as constants from '../constants';
+
+describe('actions', () => {
+  it('should increment the enthusiasm level', () => {
+    const expectedAction: IncrementEnthusiasm = {
+      type: constants.INCREMENT_ENTHUSIASM
+    };
+    expect(actions.incrementEnthusiasm()).toEqual(expectedAction);
+  });
+
+  it('should decrement the enthusiasm level', () => {
+    const expectedAction: DecrementEnthusiasm = {
+      type: constants.DECREMENT_ENTHUSIASM
+    };
+    expect(actions.decrementEnthusiasm()).toEqual(expectedAction);
+  });
+});
